f0bb801be0ad401cc7f7b168751e5a5ee71cac26
Rahul <rahulmadagoud16@gmail.com>
2026-06-02 21:41:30 +0530

    Initial commit

--- src/data/experience.js
+++ src/data/experience.js
@@ -11,16 +11,5 @@ export const experience = [
       'Participated in code reviews and contributed to team best practices'
     ]
   },
-  {
-    id: 2,
-    company: 'Startup XYZ',
-    role: 'Web Development Intern',
-    duration: 'Jan 2025 - May 2025',
-    achievements: [
-      'Built and maintained company website serving 10,000+ monthly visitors',
-      'Implemented SEO best practices resulting in 60% increase in organic traffic',
-      'Created reusable component library reducing development time by 30%',
-      'Fixed critical bugs and improved overall user experience'
-    ]
-  }
+  
 ]
